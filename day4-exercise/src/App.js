56b08d9e9c5fe21e3e9f86bf8adc6d68a1c4514b
Eddie CooRo <1300100eddiecooro@gmail.com>
2019-07-18 12:09:34 +0430

    Add exercise 2

--- day4-exercise/src/App.js
+++ day4-exercise/src/App.js
@@ -10,8 +10,8 @@ import { Example as Exercise4 } from './Exercises/App';
 function App() {
   return (
     <div className={styles.page}>
-      <Exercise1 />
-      {/* <Exercise2 /> */}
+      {/* <Exercise1 /> */}
+      <Exercise2 />
       {/* <Exercise3 /> */}
       {/* <Exercise4 /> */}
     </div>
