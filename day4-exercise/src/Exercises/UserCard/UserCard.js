8a66029910d8d04ae5952dbe5b611ae42560177f
Eddie CooRo <1300100eddiecooro@gmail.com>
2019-07-19 10:34:06 +0430

    Complete day1 exercise solutions

--- day4-exercise/src/Exercises/UserCard/UserCard.js
+++ day4-exercise/src/Exercises/UserCard/UserCard.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import families from '../../data.json';
+import styles from './UserCard.module.scss';
 import UserSelfCard from './UserSelfCard';
+import { getPersonById } from '../../utils.js';
 
 const UserCard = ({ user }) => {
-  return null;
+  return (
+    <div className={styles.container}>
+      <UserSelfCard user={user} />
+      <div className={styles.card}>
+        <p className={styles.title}>Married to</p>
+        {user.marriedTo.map(pId => (
+          <UserSelfCard
+            hoverable={true}
+            secondary={true}
+            user={getPersonById(families, pId)}
+          />
+        ))}
+      </div>
+      <div className={styles.card}>
+        <p className={styles.title}>Children</p>
+        {user.children.map(pId => (
+          <UserSelfCard
+            hoverable={true}
+            secondary={true}
+            user={getPersonById(families, pId)}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default UserCard;
--- day4-exercise/src/Exercises/UserCard/UserSelfCard.js
+++ day4-exercise/src/Exercises/UserCard/UserSelfCard.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import classnames from 'classnames';
+import styles from './UserSelfCard.module.scss';
 
-const UserSelfCard = ({ user }) => {
+const UserSelfCard = ({ user, secondary = false, hoverable = false }) => {
+  return (
+    <div className={styles.container}>
+      <div
+        className={classnames(styles.card, styles.selfCard, {
+          [styles.secondary]: secondary,
+          [styles.hoverable]: hoverable
+        })}>
+        <img className={styles.image} src={user.image} />
+        <div className={styles.imageDescription}>
+          <p>{user.name}</p>
+          <p>{user.surName}</p>
+        </div>
+        <div className={styles.details}>
+          <span>{user.gender}</span>
+          <span className={styles.bullet} />
+          <span>{user.age}</span>
+        </div>
+      </div>
+    </div>
+  );
   return null;
 };
 export default UserSelfCard;
