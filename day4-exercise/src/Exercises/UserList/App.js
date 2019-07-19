2dcc9a4d343ba0048de351fe00a3b6deba155868
Eddie CooRo <1300100eddiecooro@gmail.com>
2019-07-19 12:58:55 +0430

    Solve exercise 3

--- day4-exercise/src/Exercises/UserList/App.js
+++ day4-exercise/src/Exercises/UserList/App.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import loadingIndicator from '../../assets/images/loading.svg';
+import styles from './App.module.scss';
 import UserList from './UserList';
 
 const App = () => {
-  return null;
+  const [users, setUsers] = React.useState([]);
+  const [link, setLink] = React.useState(
+    'http://localhost:3001/persons?_page=1&_limit=2'
+  );
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    if (link) {
+      setLoading(true);
+      fetch(link)
+        .then(res => res.json())
+        .then(res => {
+          setUsers(users => [...users, ...res.data]);
+          setLoading(false);
+          setLink(res.pagination.next);
+        });
+    }
+  }, [link]);
+
+  return (
+    <>
+      <UserList users={users} />
+      {loading ? (
+        <div className={styles.container}>
+          <img src={loadingIndicator} />
+        </div>
+      ) : null}
+    </>
+  );
 };
 
 export default App;
--- day4-exercise/src/Exercises/UserList/UserList.js
+++ day4-exercise/src/Exercises/UserList/UserList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { UserSelfCard } from '../UserCard';
 
 const UserList = ({ users }) => {
-  return null;
+  return users.map(user => <UserSelfCard user={user} hoverable={true} />);
 };
 
 export default UserList;
